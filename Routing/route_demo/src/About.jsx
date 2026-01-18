025c3e8a81709a536da2b934b34ec2fa5bce8678
Rohit Kudande <rohitkudande8@gmail.com>
2026-01-18 09:46:06 +0530

    done

--- Routing/route_demo/src/About.jsx
+++ Routing/route_demo/src/About.jsx
@@ -1,8 +1,36 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
 export default function About() {
+
+  const [n1, setN1] = useState(0);
+  const [n2, setN2] = useState(0);
+  const [ans, setAns] = useState(0);
+
+  const add = () => {
+    setAns(Number(n1) + Number(n2));
+  };
 
   return (
     <>
-    <h1>This is About Page</h1>
+      <Form.Control
+        type="number"
+        onChange={(e) => setN1(e.target.value)}
+        placeholder="Enter Number 1"
+      />
+      <br />
+
+      <Form.Control
+        type="number"
+        onChange={(e) => setN2(e.target.value)}
+        placeholder="Enter Number 2"
+      />
+      <br />
+
+      <Button variant="danger" onClick={add}>Sum</Button>
+
+      <h1>{ans}</h1>
     </>
-  )
+  );
 }
--- Routing/route_demo/src/Home.jsx
+++ Routing/route_demo/src/Home.jsx
@@ -1,14 +1,20 @@
 
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 export default function Home() {
-
-    function test(){
-        alert("You Clicked Me...");
+  const [nm,SetNm]=useState("") 
+  let getnm=(e)=>{
+    SetNm(e.target.value)
+  }   
+  function test(){
+        alert("You Clicked Me..."+nm);
     }
   return (
     <>
     <h1>This is Home Page</h1>
+    Name:<input type='text' onChange={getnm}></input><br></br>
      <Button variant="success" onClick={test}>Success</Button>
+     <h1>{nm}</h1>
     </>
   )
 }
